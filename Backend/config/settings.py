dd251a00e33c5b1a0214c774b40a80f558aa521e
Irene Macha <irenemagige548@gmail.com>
2026-07-17 13:59:12 +0300

    Connect Django to Railway PostgreSQL

--- Backend/config/settings.py
+++ Backend/config/settings.py
@@ -1,5 +1,6 @@
 from datetime import timedelta
 from pathlib import Path
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -62,10 +63,10 @@ TEMPLATES = [
 WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
