dae80751d88a6f7379017d3201966ad93aa09b44
Irene Macha <irenemagige548@gmail.com>
2026-07-31 21:10:32 +0300

    Include notification IDs in response

--- Backend/students/views.py
+++ Backend/students/views.py
@@ -100,6 +100,7 @@ def my_notifications(request):
 
         data = [
             {
+                "id": n.id,
                 "title": n.title,
                 "message": n.message,
                 "read": n.is_read,
